07c0e69c51afd8a754644e4e43e1a8dc94074943
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-01-09 14:02:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/auth')
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', authRoutes)
+app.use('/api/auth', authRoutes)
 
 app.use((req, res, next) => {
     res.status(404).send()
