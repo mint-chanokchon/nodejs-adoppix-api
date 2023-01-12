386d92955fd06057b4a507f8b5761155e470a672
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-01-12 11:08:13 +0700

    feat: add upload file service

--- services/uploadFile.js
+++ services/uploadFile.js
@@ -1,24 +1,30 @@
 const Ftp = require('ftp')
 require('dotenv').config()
 
-const ftp = new Ftp()
-
 const config = { host: undefined, user: undefined, password: undefined }
 
 exports.usePublic = () => {
-    config.host = '119.59.96.90'
-    config.user = 'adopcloud_public'
-    config.password = 'Dpl3j^593'
-    console.log(process.env.SMTP_PORT)
+    config.host = process.env.PUBLIC_FTP_HOST
+    config.user = process.env.PUBLIC_FTP_USER
+    config.password = process.env.PUBLIC_FTP_PASS
+    
+    return { upload: upload }
 }
 
-exports.upload = () => {
-    console.log(config)
+function upload(fileBuffer, fileName) {
+    // check config
     if (!config.host || !config.user || !config.password) throw new Error('please config usePublic or usePrivate')
-    
+
+    // create instance of ftp
+    const ftp = new Ftp()
     ftp.connect(config)
 
+    // when instance is ready
     ftp.on('ready', () => {
-
+        // upload with array buffer
+        ftp.put(Buffer.from(fileBuffer), `${fileName}`, (err) => {
+            if (err) throw new Error(err)
+            ftp.end()
+        })
     })
 }
